5fae5076f312ec88a7b17c87fdf622fd18a0dd15
zverev <alexander.zverev@gmail.com>
2014-08-07 05:22:55 +0400

    authenticate with vk

--- client.js
+++ client.js
@@ -5,14 +5,28 @@ var getCookie = function(name) {
     return matches ? decodeURIComponent(matches[1]) : undefined;
 };
 
+var requestUserInfo = function(uid, at, callback) {
+    var script = document.createElement('SCRIPT');
+    script.src = 'https://api.vk.com/method/users.get?user_id=' + uid + '&v=5.24&access_token=' + at + '&callback=callbackFunc';
+    window.callbackFunc = function(data) {
+        callback(data);
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
 $(document).ready(function() {
     $('.btn-login').hide();
     $('.btn-logout').hide();
+    $('.username-label').hide();
     $.ajax('config.json').then(function(config) {
         var dataRef = new Firebase(config.firebase_url);
 
         $('.btn-login').click(function(e) {
-            window.location.href = '/authvk';
+            window.location.href = 'https://oauth.vk.com/authorize?' +
+                'client_id=' + config.vk_app_id + '&' +
+                'scope=email&' +
+                'redirect_uri=' + config.host + '/authvk&' +
+                'response_type=code&v=5.24';
         });
         $('.btn-logout').click(function(e) {
             dataRef.unauth();
@@ -27,16 +41,23 @@ $(document).ready(function() {
                     console.log('Login Failed!', error);
                     $('.btn-login').show();
                     $('.btn-logout').hide();
+                    $('.username-label').hide();
                 } else {
                     console.log('Authenticated successfully with payload:', result.auth);
                     console.log('Auth expires at:', new Date(result.expires * 1000));
-                    $('.btn-login').hide();
-                    $('.btn-logout').show();
+                    var payload = JSON.parse(result.auth);
+                    requestUserInfo(payload.user_id, payload.access_token, function(data) {
+                        $('.btn-login').hide();
+                        $('.btn-logout').show();
+                        $('.username-label').show();
+                        $('.username-label').html(data.response[0].first_name + ' ' + data.response[0].last_name);
+                    });
                 }
             });
         } else {
             $('.btn-login').show();
             $('.btn-logout').hide();
+            $('.username-label').hide();
         }
     });
 });
--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 var config = require('./config.json');
 
 app.get('/authvk', function(req, res) {
-    // Generate a new secure JWT
-    var FirebaseTokenGenerator = require("firebase-token-generator");
-    var tokenGenerator = new FirebaseTokenGenerator(config.firebase_secret);
-    var token = tokenGenerator.createToken({
-        some: "arbitrary",
-        data: "here"
-    });
-    res.cookie('firetoken', token);
-    res.redirect('/');
+    // сюда нас редиректит vk с параметром code
+    var code = req.query.code;
+    if (code) {
+        var requestUrl = 'https://oauth.vk.com/access_token?' +
+            'client_id=' + config.vk_app_id + '&' +
+            'client_secret=' + config.vk_secret + '&' +
+            'code=' + code + '&' +
+            'redirect_uri=' + config.host + '/authvk';
+        // отправляем запрос сервер-сервер с секретным ключом,
+        // чтобы получить access_token
+        request(requestUrl, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // теперь можно сгенерировать токен для firebase, который
+                // мы и отправим на клиент в куке
+                var FirebaseTokenGenerator = require("firebase-token-generator");
+                var tokenGenerator = new FirebaseTokenGenerator(config.firebase_secret);
+                var token = tokenGenerator.createToken(body);
+                res.cookie('firetoken', token);
+                res.redirect('/');
+            } else {
+                console.log(response);
+                res.send('error');
+            }
+        });
+    } else {
+        res.send('invalid code');
+    }
 });
 
 app.use("/", express.static(__dirname));
